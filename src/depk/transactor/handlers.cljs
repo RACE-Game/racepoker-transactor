c2dedb200dcb2d18b39cc54ddb839e0b7a22daef
team_race <race.game.team@gmail.com>
2022-03-10 16:23:31 +0800

    add ws handlers

--- src/depk/transactor/handlers.cljs
+++ src/depk/transactor/handlers.cljs
@@ -2,6 +2,7 @@
   (:require
    [depk.transactor.log :as log]
    [clojure.string :as str]
+   [cljs.core.async :as a]
    [depk.transactor.util :refer [<!? def-async-handler]]
    [depk.transactor.game :as game]
    [solana-clj.publickey :as pubkey]
@@ -135,7 +136,6 @@
   [req]
   {:status 200, :body "ok"})
 
-
 ;; Websocket Event Handler
 
 (defmulti event-msg-handler :id)
@@ -145,3 +145,137 @@
   (log/debugf "Unhandled event: %s" ev-msg)
   (when ?reply-fn
     (?reply-fn {:unmatched-event-as-echoed-from-server ev-msg})))
+
+(defmethod event-msg-handler :game/attach
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Attach game: %s" uid)
+  (let [[game-id player-id] uid]
+    (a/go
+     (if (a/<! (game/attach-game @game-manager game-id player-id))
+       (?reply-fn {:result :ok})
+       (?reply-fn {:result :err})))))
+
+(defmethod event-msg-handler :game/state
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Sync game state: %s" uid)
+  (a/go
+   (let [[game-id player-id] uid
+         state (a/<! (game/state @game-manager game-id))]
+     (?reply-fn {:result :ok,
+                 :state  state}))))
+
+(defmethod event-msg-handler :game/leave
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Leave game: %s" uid)
+  (a/go
+   (let [[game-id player-id]     uid
+         {:keys [released-keys]} ?data]
+     (a/<! (game/leave @game-manager game-id player-id released-keys))
+     (?reply-fn {:result :ok}))))
+
+(defmethod event-msg-handler :game/alive
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Keep alive: %s" uid)
+  (a/go
+   (let [[game-id player-id] uid]
+     (a/<! (game/alive @game-manager game-id player-id))
+     (?reply-fn {:result :ok}))))
+
+(defmethod event-msg-handler :client/shuffle-cards
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Client shuffle cards: %s" uid)
+  (a/go
+   (let [[game-id player-id] uid
+         {:keys [data]}      ?data]
+     (a/<! (game/shuffle-cards @game-manager game-id player-id data))
+     (?reply-fn {:result :ok}))))
+
+(defmethod event-msg-handler :client/encrypt-cards
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Client encrypt cards: %s" uid)
+  (a/go
+   (let [[game-id player-id] uid
+         {:keys [data]}      ?data]
+     (a/<! (game/encrypt-cards @game-manager game-id player-id data))
+     (?reply-fn {:result :ok}))))
+
+(defmethod event-msg-handler :client/share-keys
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Client share keys: %s" uid)
+  (a/go
+   (let [[game-id player-id]  uid
+         {:keys [share-keys]} ?data]
+     (a/<! (game/share-keys @game-manager game-id player-id share-keys))
+     (?reply-fn {:result :ok}))))
+
+(defmethod event-msg-handler :client/release
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Client release keys: %s" uid)
+  (a/go
+   (let [[game-id player-id]     uid
+         {:keys [released-keys]} ?data]
+     (a/<! (game/release @game-manager game-id player-id released-keys))
+     (?reply-fn {:result :ok}))))
+
+(defmethod event-msg-handler :player/call
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Call: %s" uid)
+  (a/go
+   (let [[game-id player-id] uid]
+     (a/<! (game/player-call @game-manager game-id player-id))
+     (?reply-fn {:result :ok}))))
+
+(defmethod event-msg-handler :player/raise
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Raise: %s" uid)
+  (a/go
+   (let [[game-id player-id] uid
+         {:keys [amount]}    ?data]
+     (a/<! (game/player-raise @game-manager game-id player-id amount))
+     (?reply-fn {:result :ok}))))
+
+(defmethod event-msg-handler :player/check
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Check: %s" uid)
+  (a/go
+   (let [[game-id player-id] uid]
+     (a/<! (game/player-check @game-manager game-id player-id))
+     (?reply-fn {:result :ok}))))
+
+(defmethod event-msg-handler :player/bet
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Bet: %s" uid)
+  (a/go
+   (let [[game-id player-id] uid
+         {:keys [amount]}    ?data]
+     (a/<! (game/player-bet @game-manager game-id player-id amount))
+     (?reply-fn {:result :ok}))))
+
+(defmethod event-msg-handler :player/fold
+  [{:as ev-msg, :keys [event id uid ?data ring-req ?reply-fn send-fn]}]
+  (log/debugf "Fold: %s" uid)
+  (a/go
+   (let [[game-id player-id]  uid
+         {:keys [share-keys]} ?data]
+     (a/<! (game/player-fold @game-manager game-id player-id share-keys))
+     (?reply-fn {:result :ok}))))
+
+(defn attach-event-handler
+  [ch-recv]
+  (a/go-loop [evt (a/<! ch-recv)]
+    (try
+      (event-msg-handler evt)
+      (catch js/Error e (log/error e)))
+    (recur (a/<! ch-recv))))
+
+(defn user-id-fn
+  [{:keys [params]}]
+  (let [{:keys [pubkey sig game-id]} params
+        k   (pubkey/to-buffer (pubkey/make-public-key pubkey))
+        msg (buffer/Buffer.from
+             (str pubkey
+                  " sign with game "
+                  game-id
+                  " for RACE Poker."))]
+    (when (nacl/sign.detached.verify msg sig k)
+      [game-id pubkey])))
--- src/depk/transactor/state/websocket.cljs
+++ src/depk/transactor/state/websocket.cljs
@@ -1,33 +1,33 @@
 (ns depk.transactor.state.websocket
   (:require
-   [mount.core          :as mount]
+   [mount.core :as mount]
    [taoensso.sente.server-adapters.macchiato :refer [make-macchiato-channel-socket-server!]]
    [taoensso.sente]
    [depk.transactor.handlers :as h]
    [depk.transactor.log :as log]
-   [cljs.core.async     :as a]))
+   [cljs.core.async :as a]
+   [solana-clj.publickey :as pubkey]
+   ["tweetnacl" :as nacl]
+   ["buffer" :as buffer]))
 
-(defn user-id-fn
-  [{:keys [params]}]
-  (:pubkey params))
+(def lock_ (atom nil))
 
 (defn make-websocket
   []
-  (let [{:keys [ch-recv send-fn connected-uids
-                ajax-post-fn ajax-get-or-ws-handshake-fn]}
-        (make-macchiato-channel-socket-server! {:csrf-token-fn nil,
-                                                :user-id-fn    user-id-fn})]
-    (log/info "Start websocket...")
-    (a/go-loop [evt (a/<! ch-recv)]
-      (try
-        (h/event-msg-handler evt)
-        (catch js/Error e (log/errorf e)))
-      (recur (a/<! ch-recv)))
-    {:ring-ajax-post ajax-post-fn,
-     :ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn,
-     :ch-chsk        ch-recv,
-     :chsk-send!     send-fn,
-     :connected-uids connected-uids}))
+  (when-not @lock_
+    (reset! lock_ true)
+    (let [{:keys [ch-recv send-fn connected-uids
+                  ajax-post-fn ajax-get-or-ws-handshake-fn]}
+          (make-macchiato-channel-socket-server!
+           {:csrf-token-fn nil, ; skip CSRF check, no page hosted
+            :user-id-fn    h/user-id-fn})]
+      (log/info "Start websocket...")
+      (h/attach-event-handler ch-recv)
+      {:ring-ajax-post ajax-post-fn,
+       :ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn,
+       :ch-chsk        ch-recv,
+       :chsk-send!     send-fn,
+       :connected-uids connected-uids})))
 
 (mount/defstate websocket
   :start
